492220167317b2032ae864f08d6e6321e7766d54
Animesh-Ghosh <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-03-05 08:30:22 -0800

    Update files
    Updating files, adding image resources.

--- Script/myScript.js
+++ Script/myScript.js
@@ -5,3 +5,19 @@ function hello () {
 function bye () {
 	document.getElementById ('bye').innerHTML = Date ();
 }
+
+function helloConfirm () {
+	var txt;
+	var r = window.confirm ("Delete this file?");
+	if (r == true) txt = "OK Pressed!";
+	else txt = "Cancel Pressed!";
+	document.getElementById ("confirm").innerHTML = txt;
+}
+
+function bulbON () {
+	document.getElementById ("myImage").src = "Images/pic_bulbon.gif";
+}
+
+function bulbOFF () {
+	document.getElementById ("myImage").src = "Images/pic_bulboff.gif";
+}
